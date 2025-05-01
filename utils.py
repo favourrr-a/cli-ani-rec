5811be1169c74f25a8ff810c49d107e1a7237661
favourrr-a <favourackonu13@gmail.com>
2025-05-01 11:08:19 +0000

    added helper functions

---
+++ utils.py
@@ -0,0 +1,46 @@
+import pickle
+import csv
+from classes import Anime, GraphNode, Graph
+
+def save_object(obj, file_name:str):
+    if not isinstance(file_name, str):
+        raise TypeError("Input type must be a string")
+    try:
+        with open(file_name, "wb") as object_f:
+            pickle.dump(obj, object_f)
+        object_f.close()
+    except Exception as e:
+        print("Failed to save object", e)
+
+def load_object(file_name:str):
+    if not isinstance(file_name, str):
+        raise TypeError("Input type must be a string")
+    try:
+        with open(file_name, "rb") as object_f:
+            return pickle.load(object_f)
+    except FileNotFoundError:
+        print("Input file not found")
+    except Exception as e:
+        print("Failed to load object", e)
+
+def csv_to_list(file_name):
+    anime_list = []
+    with open(file_name, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  
+        for row in reader:
+            anime = Anime(row[0], set(row[1:]))
+            anime_list.append(anime)
+    return anime_list
+
+def create_anime_graph(anime_file, graph_name):
+    anime_list = csv_to_list(anime_file)
+
+    anime_nodes = [GraphNode(anime) for anime in anime_list]
+
+    for anime_node in anime_nodes:
+        anime_node.add_neighbours(anime_nodes)
+
+    anime_graph = Graph(anime_nodes)
+    save_object(anime_graph, graph_name)
+    return graph_name
